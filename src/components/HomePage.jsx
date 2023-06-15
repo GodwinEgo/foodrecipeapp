7f6034a34fa3d1d91fea838ed88ebf1cfbc2421a
MrMoney001 <eego12745@gmail.com>
2023-06-15 21:58:36 +0100

    home

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './HomePage.css';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import slide_1 from '../components/Images/slide_1.jpg'
+import slide_2 from '../components/Images/slide_2.jpg'
+import slide_3 from '../components/Images/slide_3.jpg'
+
+
+
+
+
+
+
+const divStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundSize: 'cover',
+  height: '100vh'
+}
+const slideImages = [
+  {
+    url: slide_1,
+    caption: 'Slide 1'
+  },
+  {
+    url: slide_2,
+    caption: 'Slide 2'
+  },
+  {
+    url: slide_3,
+    caption: 'Slide 3'
+  },
+];
 
 const HomePage = () => {
-  return (
-    <div className="home-page stone">
-     
-    </div>
-  );
-};
+    return (
+      <div className="slide-container">
+        <Slide>
+         {slideImages.map((slideImage, index)=> (
+            <div key={index}>
+              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+      </div>
+    )
+}
 
-export default HomePage;
+export default HomePage
